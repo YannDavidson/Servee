a22b836471913b6c90607acb1f20d3ac0f6f8a83
Piscou Balthazar <30988886+YannDavidson@users.noreply.github.com>
2026-07-17 16:10:23 -0500

    feat(api): enable request validation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
@@ -6,6 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true, credentials: true });
   app.setGlobalPrefix("api");
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
   await app.listen(process.env.PORT ?? 4000);
 }
 
